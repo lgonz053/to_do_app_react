6e02b9d02ea3fba751958cd767145ea125833427
Leo Gonzalez <lgonz053@gmail.com>
2019-04-15 16:38:43 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+ function App() {
+  return (
+        <div>
+          <input type="checkbox"/>Mow the Lawn
+          <input type="checkbox"/>Spend time with Tara
+          <input type="checkbox"/>Sweep the house
+          <input type="checkbox"/>Code some code
+        </div>
+    )
 }
 
 export default App;
---
+++ src/components/MainContent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MainContent() {
+  return (
+        <div>
+          <h1>This is my info in here</h1>
+        </div>
+    
+    )
+}
+
+export default MainContent
