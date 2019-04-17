9170ce4ddee7940c476a91e26dac38b337d8fc3f
Leo Gonzalez <lgonz053@gmail.com>
2019-04-17 15:53:40 -0500

    added data to mimic json data

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-function ToDoItem() {
+function ToDoItem(props) {
   return (
       <div className='todo-item'>
-        <input type='checkbox'/>
-        <p>Mow the Lawn</p>
+        <input type="checkbox"/>
+        <h1>{props.text}</h1>
+        <h2>{props.completed}</h2>
       </div>
     )
 }
