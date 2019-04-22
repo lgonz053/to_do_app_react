8c2d85f2fe6b5081cbc9c8627e60bf5c5247bcda
Leo Gonzalez <lgonz053@gmail.com>
2019-04-22 14:07:57 -0500

    added class components

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import Footer from './components/Footer'
 import ToDoItem from './components/ToDoItem'
 import ToDosData from './components/ToDosData'
 
- function App() {
-  const items = ToDosData.map(item => <ToDoItem key={item.id} text={item.text} completed={item.completed} />)
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      tasks: ToDosData.map(item => <ToDoItem key={item.id} text={item.text} completed={item.completed} />)
+    }
+  }
 
-  return (
-          <div className='todo-list'>
-            {items}
-          </div>
-  )
+  render() {
+    return (
+            <div className='todo-list'>
+              {this.state.tasks}
+            </div>
+    )
+  }
 }
 
 export default App;
